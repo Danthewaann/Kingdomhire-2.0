3d4d4034eb60592c1dd0e2143cdd428748d6b41f
danielcrblack <danielcrblack@gmail.com>
2018-03-04 16:19:01 +0000

    Update web routes + rename User seeder class

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,17 @@ Route::get('/', 'PagesController@welcome');
 Route::get('/vehicles', 'PagesController@vehicles');
 Route::get('/contact', 'PagesController@contact');
 
-Auth::routes();
-
+/* Administrator routes */
 Route::get('/admin', 'AdminController@index');
+
+/* Login routes */
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+
+/* Password reset routes */
+$this->get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+$this->post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+$this->get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+$this->post('password/reset', 'Auth\ResetPasswordController@reset');
+

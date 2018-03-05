cb284002447f388d80ba1c6f239b4b23bb8710a7
danielcrblack <danielcrblack@gmail.com>
2018-03-05 20:27:27 +0000

    Slight change to routes

--- routes/web.php
+++ routes/web.php
@@ -12,21 +12,21 @@
 */
 
 /* Basic page routes */
-Route::get('/', 'PagesController@welcome');
-Route::get('/vehicles', 'PagesController@vehicles');
-Route::get('/contact', 'PagesController@contact');
+Route::get('/', 'PagesController@home')->name('home');
+Route::get('/vehicles', 'PagesController@vehicles')->name('vehicles');
+Route::get('/contact', 'PagesController@contact')->name('contact');
 
 /* Administrator routes */
 Route::get('/admin', 'AdminController@index');
 
 /* Login routes */
-Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
-Route::post('login', 'Auth\LoginController@login');
-Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+Route::get('/login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('/login', 'Auth\LoginController@login');
+Route::post('/logout', 'Auth\LoginController@logout')->name('logout');
 
 /* Password reset routes */
-Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
-Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
-Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
-Route::post('password/reset', 'Auth\ResetPasswordController@reset');
+Route::get('/password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('/password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('/password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('/password/reset', 'Auth\ResetPasswordController@reset');
 

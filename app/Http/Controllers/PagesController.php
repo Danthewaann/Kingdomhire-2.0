a98c72b03527701b91ca2a630f8e59dc4013d8e3
danielcrblack <danielcrblack@gmail.com>
2018-03-06 23:13:33 +0000

    Created basic home page

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -20,4 +20,9 @@ class PagesController extends Controller
     {
         return view('contact');
     }
+
+    public function home()
+    {
+        return view('home');
+    }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,42 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: daniel
- * Date: 04/03/18
- * Time: 17:30
- */
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+<div class="row">
+	<div class="col-md-4">
+		<h3>What is Kingdom Car Hire?</h3>
+		<p>Kingdom Car Hire is a self-employed, one-man business owned and ran by proprietor Keith Black. The business specialises in vehicle hire and repair.</p>
+		<p>Keith's experience spans over 40 years of working in the motor industry, so you can expect high quality service.</p>
+		<p>Kingdom Car Hire is located and based just outside Markethill, Co. Armagh. Exact directions can be found on our Contact page.</p>
+		<div id="table"></div>
+	</div>
+	<div class="col-md-4">
+		<h3>What we provide to you</h3>
+		<p>Kingdom Car Hire cater for general, public and business needs. Whatever you need, we are sure we can help you out.</p>
+		<p>We have a wide selection of vehicles to choose from. We provide hatchbacks, 4-door salons, people carriers, small vans and large vans.</p>
+		<p>Our fleet is ever expanding to include more diverse vehicles, and we ensure that our vehicles are reliable and maintained, so make sure to get in
+			touch to see if we have what you want</p>
+	</div>
+	<div class="col-md-4">
+	  <h3>Opening hours &amp; reservations</h3>
+	  <p>You need to call us to be able to reserve a vehicle.</p>
+	  <p>Make sure to reserve your vehicle at least 1 day before expected pickup</p>
+	  <table class="table" style="width: 100%;">
+		<tr>
+			<th>Weekdays</th>
+			<td>9am - 6pm</td>
+		</tr>
+		<tr>
+			<th>Saturday</th>
+			<td>9am - 1pm</td>
+		</tr>
+		<tr>
+			<th>Sunday</th>
+			<td>Closed</td>
+		</tr>
+	  </table>
+
+	</div>
+  </div>
+</div>
+@endsection

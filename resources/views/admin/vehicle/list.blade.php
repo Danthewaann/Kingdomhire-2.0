acf3a5e34126112725ab74423ce6ce1cb2a36772
danielcrblack <danielcrblack@gmail.com>
2018-04-15 14:01:03 +0100

    Added is_active column to vehicle list component

--- resources/views/admin/vehicle/list.blade.php
+++ resources/views/admin/vehicle/list.blade.php
@@ -2,10 +2,16 @@
 <table class="table">
     <th>Vehicle</th>
     <th>Status</th>
+    <th>Active</th>
     @foreach($vehicles as $vehicle)
         <tr>
             <td style="width: 250px;">{{ $vehicle->make }} {{ $vehicle->model }}</td>
             <td>{{ $vehicle->status }}</td>
+            @if($vehicle->is_active == true)
+                <td>Yes</td>
+            @else
+                <td>No</td>
+            @endif
         </tr>
     @endforeach
 </table>

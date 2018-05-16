6b6f31edca7381af615c04970c7a72352b38b521
daniel <danielcrblack@gmail.com>
2018-05-16 23:22:43 +0100

    Updated reservation list component
    Took the content of the list-table-row component, and put it inside the reservation list component

--- resources/views/admin/reservation/list.blade.php
+++ resources/views/admin/reservation/list.blade.php
@@ -1,35 +1,40 @@
 <div class="panel panel-default">
   <div class="panel-heading">
-  @if(!empty($vehicle))
-    @if(!$vehicle->reservations->isEmpty())
-      <h3>Active reservations</h3>
-    @else
-      <h3>No active reservations</h3>
-    @endif
-  @else
     @if(!$reservations->isEmpty())
       <h3>Active reservations</h3>
     @else
       <h3>No active reservations</h3>
     @endif
-  @endif
   </div>
-  @if((!empty($reservations) and count($reservations) > 0) or ( !empty($vehicle) and count($vehicle->reservations) > 0))
+  @if((!$reservations->isEmpty()))
     <div class="panel-body">
       <div class="table-responsive">
         <table class="table">
           <thead>
           <tr>
-            @if(!empty($vehicles) or !empty($reservations))
-              <th>Vehicle</th>
-            @endif
+            <th>Vehicle</th>
             <th>Start Date</th>
             <th>End Date</th>
             <th></th>
             <th></th>
           </tr>
           </thead>
-          @include('admin.reservation.list-table-row')
+          @foreach($reservations as $reservation)
+            <tr>
+              <td><a href="{{ route('vehicle.show', ['make' => $reservation->vehicle->make, 'model' => $reservation->vehicle->model, 'id' => $reservation->vehicle->id]) }}">{{ $reservation->vehicle->name() }}</a></td>
+              <td>{{ $reservation->start_date }}</td>
+              <td>{{ $reservation->end_date }}</td>
+              <td>
+                {{ Form::open(['route' => ['reservation.cancel', $reservation->id], 'method' => 'delete']) }}
+                {{ Form::submit('Cancel', ['class' => 'btn btn-primary']) }}
+                {{ Form::close() }}
+              </td>
+              <td>
+                <a href="{{ route('reservation.editForm', ['make' => $reservation->vehicle->make, 'model' => $reservation->vehicle->model, 'vehicle_id' => $reservation->vehicle->id, 'reservation_id' => $reservation->id]) }}"
+                   class="btn btn-primary" role="button" aria-pressed="true">Re-schedule</a>
+              </td>
+            </tr>
+          @endforeach
         </table>
       </div>
     </div>

39f1f9b4a59326b61c2528a80b3501b37cfadb90
daniel <danielcrblack@gmail.com>
2018-05-29 18:01:05 +0100

    Added basic navigation tests

---
+++ tests/Feature/PublicNavigationTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class PublicNavigationTest extends TestCase
+{
+    public function testHome()
+    {
+        $response = $this->get('/');
+        $response->assertStatus(200);
+    }
+
+    public function testVehicles()
+    {
+        $response = $this->get('/vehicles');
+        $response->assertStatus(200);
+    }
+
+    public function testContact()
+    {
+        $response = $this->get('/contact');
+        $response->assertStatus(200);
+    }
+}

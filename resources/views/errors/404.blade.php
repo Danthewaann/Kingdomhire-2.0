1a5e6f12db9d7e90f3ecab8cedbef82118cc3468
danielcrblack <danielcrblack@gmail.com>
2018-12-18 19:54:39 +0000

    Fixed image n/a line height

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.public')
+
+@section('content')
+  <div class="jumbotron jumbotron-header">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <h1>Page Not Found</h1>
+        </div>
+      </div>
+    </div>
+  </div>
+@endsection

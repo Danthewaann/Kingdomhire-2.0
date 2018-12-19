c3fd46e2d77e258cf319e7af952db265ee4a9558
danielcrblack <danielcrblack@gmail.com>
2018-12-19 11:32:16 +0000

    Made collapsed hamburger consistent

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -26,10 +26,9 @@
           <!-- Collapsed Hamburger -->
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse" aria-expanded="false">
               <span class="sr-only">Toggle Navigation</span>
-              <span class="glyphicon glyphicon-menu-hamburger"></span>
-              {{--<span class="icon-bar"></span>--}}
-              {{--<span class="icon-bar"></span>--}}
-              {{--<span class="icon-bar"></span>--}}
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
             </button>
           </div>
         </div>

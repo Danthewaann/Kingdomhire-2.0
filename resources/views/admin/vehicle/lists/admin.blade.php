12a6317c617f17fd15d840def9038c0d95fcaedd
danielcrblack <danielcrblack@gmail.com>
2018-12-20 21:29:53 +0000

    More changes to vehicle img sizes

--- resources/views/admin/vehicle/lists/admin.blade.php
+++ resources/views/admin/vehicle/lists/admin.blade.php
@@ -1,4 +1,4 @@
-<div class="col-md-2 col-sm-3 col-xs-12">
+<div class="col-lg-2 col-md-4 col-sm-3 col-xs-12">
   <div class="panel panel-default">
     <div class="panel-heading">
       @if($activeVehicles->isEmpty() and $inactiveVehicles->isEmpty())
@@ -23,14 +23,14 @@
     </div>
   </div>
 </div>
-<div class="col-md-10 col-sm-9 col-xs-12">
+<div class="col-lg-10 col-md-8 col-sm-9 col-xs-12">
   @if($activeVehicles->isNotEmpty() or $inactiveVehicles->isNotEmpty())
     <div class="tab-content">
       @if($activeVehicles->isNotEmpty())
         <div id="all" class="tab-pane fade in active">
           <div class="row">
             @foreach($activeVehicles as $vehicle)
-              <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+              <div class="col-lg-6 col-md-12 col-sm-6 col-xs-12">
                 @include('admin.vehicle.summaries.admin-dashboard')
               </div>
             @endforeach
@@ -40,7 +40,7 @@
           <div id="{{ str_replace(" ", "-", array_keys($activeVehicles->groupBy('type')->toArray())[$i]) }}" class="tab-pane fade">
             <div class="row">
               @foreach($activeVehicles->groupBy('type')->slice($i, 1)->first() as $vehicle)
-                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                <div class="col-lg-6 col-md-12 col-sm-6 col-xs-12">
                   @include('admin.vehicle.summaries.admin-dashboard')
                 </div>
               @endforeach
@@ -52,7 +52,7 @@
         <div id="discontinued" class="tab-pane fade{{ $activeVehicles->isEmpty() ? ' in active' : '' }}">
           <div class="row">
             @foreach($inactiveVehicles as $vehicle)
-              <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+              <div class="col-lg-6 col-md-12 col-sm-6 col-xs-12">
                 @include('admin.vehicle.summaries.admin-dashboard')
               </div>
             @endforeach

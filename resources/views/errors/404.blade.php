232a76b3dd43a27b7d5bebbe3aeec4f1c66ed1cb
Daniel <danielcrblack@gmail.com>
2019-05-24 19:00:39 +0100

    Updated 404 page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,11 +1,15 @@
-@extends('layouts.public')
+@extends('layouts.login')
 
 @section('content')
   <div class="jumbotron jumbotron-header">
     <div class="container">
       <div class="row">
         <div class="col-md-12">
-          <h1>Page Not Found</h1>
+          <h1 style="text-align: center">Page Not Found</h1>
+          <hr>
+          <div style="text-align: center">
+            <a class="btn btn-lg btn-info" role="button" href="{{ route('public.home') }}">Return to Kingdomhire</a>
+          </div>
         </div>
       </div>
     </div>

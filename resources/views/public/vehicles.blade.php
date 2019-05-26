86f53b66ef91eeead33c6d243c616a3e67ddd53d
Daniel <danielcrblack@gmail.com>
2019-05-27 00:44:16 +0100

    Added temporary important note to public vehicles page

--- resources/views/public/vehicles.blade.php
+++ resources/views/public/vehicles.blade.php
@@ -15,6 +15,12 @@
       <div class="col-md-6">
         <p class="paragraph">Below are all the vehicles in that are in our fleet. You need to call us to be able to reserve a vehicle.
           Make sure to reserve your vehicle at least 1 day before expected pickup.</p>
+          <div class="alert alert-danger" style="margin-bottom: 0" role="alert">
+            <span style="padding: 5px 0; font-size: 22px" class="glyphicon glyphicon-info-sign"></span>&nbsp;&nbsp;<strong style="font-size: 22px">Important note!</strong><br>
+            The vehicles listed below may no longer be available.<br>
+            Kingdomhire.com is currently being revamped, and soon all our vehicles will
+            be listed on our site, so until then you can <a class="text-link" style="text-decoration: underline" href="{{ route('public.contact')}}">contact us</a> to see what we have available.<br>
+          </div>
       </div>
     </div>
   </div>

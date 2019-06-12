7d0e6f21b686b5b52b8de147c99035a4cd8da956
Daniel <danielcrblack@gmail.com>
2019-06-12 17:40:25 +0100

    Updating email views

--- resources/views/email/contact-us.blade.php
+++ resources/views/email/contact-us.blade.php
@@ -5,29 +5,34 @@
     <div class="navbar-header public-navbar-header">
         <img class="logo" src="{{ $message->embed(public_path('/static/Kingdomhire_logo.png')) }}">
     </div>
-    <h2>E-Mail Received</h2>
-    <h5>Sent through Kingdomhire email form</h5>  
     <table class="table">
         <tbody>
             <tr>
-                <th>Sent By</th>
+                <h2>E-Mail Received</h2>
+                <h5>Sent through Kingdomhire email form</h5> 
+            </tr>
+            <tr>
+                <th id="sent_by">Sent By</th>
                 <td>{{ $name }}</td>
             </tr>
             <tr>
-                <th>E-Mail</th>
-            <td><a class="text-link" href="mailto:{{ $email }}">{{ $email }}</a></td>
+                <th id="email">E-Mail</th>
+                <td><a class="text-link" href="mailto:{{ $email }}?subject=Re: {{ rawurlencode($subject) }}">{{ $email }}</a></td>
             </tr> 
         </tbody>        
     </table>
-    <div class="email-content">
-        <h2>E-Mail Content</h2>
-        <hr>
-        <p><b>Subject:</b> {{ $subject }}</p>
-        <p><b>Message:</b><br>{!! $user_message !!}</p>
-    </div>
-    <hr>
-    <a class="btn btn-lg btn-primary" role="button" href="mailto:{{ $email }}">Reply to {{ $name }}</a>
-    <hr>
-    <p>&copy; {{ date('Y') }} <a class="no-link">kingdomhire.com</a></p>    
+    <table class="table">
+        <tbody>
+            <tr><th colspan="2" style="border-top: none; border-right: none;"><h2>E-Mail Content</h2></th></tr>
+            <tr><th id="subject">Subject</th><td id="subject_td">{{ $subject }}</td></tr>
+            <tr><th id="message">Message</th><td id="message_td">{!! $user_message !!}</td></tr>
+            <tr>
+                <td style="text-align: center; padding-top: 20px; padding-bottom: 20px" colspan="2">
+                <a class="btn btn-lg btn-primary" role="button" href="mailto:{{ $email }}?subject=Re: {{ rawurlencode($subject) }}">Reply to {{ $name }}</a>
+                </td>
+            </tr>
+            <tr><td style="text-align: center; padding-top: 50px" colspan="2">&copy; {{ date('Y') }} <a class="no-link">kingdomhire.com</a></td></tr>
+        </tbody>
+    </table>
 </div>
 @endsection
--- resources/views/email/receipt.blade.php
+++ resources/views/email/receipt.blade.php
@@ -16,19 +16,8 @@
             <tr><th colspan="2" style="border-top: none; border-right: none;"><h2>E-Mail Content</h2></th></tr>
             <tr><th id="subject">Subject</th><td id="subject_td">{{ $subject }}</td></tr>
             <tr><th id="message">Message</th><td id="message_td">{!! $user_message !!}</td></tr>
-            {{-- <tr><td colspan="2">{!! $user_message !!}</td></tr> --}}
             <tr><td style="text-align: center; padding-top: 50px" colspan="2">&copy; {{ date('Y') }} <a class="no-link">kingdomhire.com</a></td></tr>
         </tbody>
     </table>
-    {{-- <div class="email-content">
-        <h2>E-Mail Content</h2>
-        <hr>
-        <p><b>Subject:</b> {{ $subject }}</p>
-        <p><b>Message:</b><br>{!! $user_message !!}</p>
-    </div> --}}
-    {{-- <hr> --}}
-    {{-- <a class="btn btn-lg btn-primary" role="button" href="mailto:{{ $email }}">Reply to {{ $name }}</a> --}}
-    {{-- <hr> --}}
-    {{-- <p>&copy; {{ date('Y') }} <a class="no-link">kingdomhire.com</a></p>     --}}
 </div>
 @endsection

1051b552009f584eac2fb18e626b1b8969a3fcd6
Daniel <danielcrblack@gmail.com>
2019-06-13 10:28:24 +0100

    Removed CSRF meta tag

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -6,8 +6,6 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="format-detection" content="telephone=no">
 
-  <!-- CSRF Token -->
-  <meta name="csrf-token" content="{{ csrf_token() }}">
   @if(Request::is('/') || Request::is('home'))
     <title>Welcome | Kingdomhire - Car & Van Rental Agency</title>
     <meta name="description" content="Car & Van Rental Agency Based in Markethill, Co. Armagh. We provide a wide range of vehicles to rent. Contact us if you are interested in renting from us.">

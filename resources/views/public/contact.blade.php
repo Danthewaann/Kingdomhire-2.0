5edf9d018faac22dcccfabf4522fcb006b497c50
Daniel <danielcrblack@gmail.com>
2019-06-18 17:05:38 +0100

    More css style changes

--- resources/views/public/contact.blade.php
+++ resources/views/public/contact.blade.php
@@ -33,7 +33,7 @@
   <div class="container">
     <div class="row">
       <div class="col-sm-12">
-        <h2 class="sub-header">Where to Find Us?</h2>
+        <h2 class="map-header">Where to Find Us?</h2>
         <p class="paragraph">For directions, you can use the map below to help find your way to Kingdomhire.</p>
         <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d9313.696456269257!2d-6.502924066660125!3d54.29645086742982!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0000000000000000%3A0x9d3b28ee1d8f52c1!2sKingdom+Car+Hire!5e0!3m2!1sen!2suk!4v1459283163122"
                 class="map">

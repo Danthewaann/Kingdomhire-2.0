050637d1ae8cb857f24dd80f24433a967a2bc637
Daniel Black <danielcrblack@gmail.com>
2020-06-15 17:39:30 +0100

    Added appropriate default values for snappy config

--- config/snappy.php
+++ config/snappy.php
@@ -35,7 +35,7 @@ return [
     
     'pdf' => [
         'enabled' => true,
-        'binary'  => env('WKHTML_PDF_BINARY', '/usr/local/bin/wkhtmltopdf-amd64'),
+        'binary'  => env('WKHTML_PDF_BINARY', base_path('vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64')),
         'timeout' => false,
         'options' => [],
         'env'     => [],
@@ -43,7 +43,7 @@ return [
     
     'image' => [
         'enabled' => true,
-        'binary'  => env('WKHTML_IMG_BINARY', '/usr/local/bin/wkhtmltoimage-amd64'),
+        'binary'  => env('WKHTML_IMG_BINARY', base_path('vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltoimage-amd64')),
         'timeout' => false,
         'options' => [],
         'env'     => [],
